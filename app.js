0b30047e895d8a4c59510b06b07a6d4a7f4d8b07
Sam Goldsmith <samggold@gmail.com>
2016-07-20 08:59:46 -0600

    it's working but maybe not how you asked

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   console.log('sanity test');
 });
+function calculateTip(sub, tip) {
+  return (sub * tip).toFixed(2);
+}
 $('form').submit(function(event) {
   event.preventDefault();
   var subTotal = parseInt($('#amount').val());
   var tipAmount = $('#howMuch').val();
-  $('.result').append('<h1 class="text-center">' + "You should tip $" + parseFloat(subTotal * tipAmount).toFixed(2) + "." + '<h1>');
-});
+  $('.result').append('<h1 class="text-center">' + "You should tip $" + calculateTip(subTotal, tipAmount) + "." + '<h1>');
 });
